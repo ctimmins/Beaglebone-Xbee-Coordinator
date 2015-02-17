f5259b58fbb601f845ebec2563cd575c7db33b54
chadtimmins <chadtimmins@gmail.com>
2015-02-17 10:55:27 -0800

    Added try-catch blocks to handle exceptions.  Moved parseData function to helpers

--- main.py
+++ main.py
@@ -42,44 +42,19 @@ if __name__ == '__main__':
 			while xbee.serial.inWaiting() == 0:
 				sleep(0.25)
 			msg = xbee.Receive()
-			msg = parse.parseXbeeMsg(msg)
-			logData(msg)
+			try:
+				msg = parse.parseXbeeMsg(msg)
+				node_id = msg["SrcAddr"]
+				# Get current time and date for timestamp of firebase data
+				time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+				print time
+				logData(msg)
+			except TypeError:
+				print "Type Error"
 
-			node_id = msg["SrcAddr"]
 			
-			# Get current time and date for timestamp of firebase data
-			time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-			#split comma separated data portion of incoming message 
-			data = msg["Data"].split(',')
-			raw_ir = data[0]
-			sCksm = data[-1]
-
-			#sensor data is remaining data
-			sdata = data[1:-1]
-			fb_payload = {}
-			for i in range(len(sdata)):
-				sd_i = sdata[i].split(':')
-				s_lev = str(i)
-				fb_payload = {
-					s_lev: {
-						"Temperature": sd_i[0],
-						"VWC": sd_i[1]
-					}
-				}
-
-			#add actual IR reading to firebase package
-			real_ir = convertIr(raw_ir)
-			fb_payload["IR"] = real_ir
-
-			#add node name/id to firebase package
-			fb_payload["Node"] = node_id
-
-			# send package to correct location/node in firebase
-			s_location = 'Winery'
-			n_url = "%s/%s" % (s_location, node_name)
-			fb.put(n_url, time, fb_payload)
 			
+				
 
 			#xbee.SendStr(str(data), src)
 			sleep(0.25)
--- xb/helpers/parse.py
+++ xb/helpers/parse.py
@@ -9,7 +9,7 @@ and returns a formatted data structure that can be used to retrieve
 data and configuration options.
 
 Received Messages Format: 
-	[0x00][LSB][SrcAddrHigh][SrcAddrLow][junk][0x00][..data..][CheckSum]
+	[0x7E][0x00][LSB][API][SrcAddrHigh][SrcAddrLow][junk][0x00][..data..][CheckSum]
 
 """
 
@@ -28,5 +28,43 @@ def parseXbeeMsg(msg):
 	}
 
 	return pkg
+
+
+"""
+Parses data and formats for firebase uploading
+"""
+def parseData(msg):
+	# split comma separated data portion of incoming message 
+	data = msg.split(',')
+	raw_ir = data[0]
+	sChksm = data[-1]
+
+	#sensor readings are the remaining data
+	sdata = data[1:-1]
+	fb_payload = {}
+	for i in range(len(sdata)):
+		sd_i = sdata[i].split(':')
+		s_lev = str(i)
+		fb_payload = {
+			s_lev: {
+				"temp": sd_i[0],
+				"vwc": sd_i[1]
+			}
+		}
+
+	return fb_payload
+	
+	#add actual IR reading to firebase package
+	real_ir = convertIr(raw_ir)
+	fb_payload["IR"] = real_ir
+
+	#add node name/id to firebase package
+	fb_payload["Node"] = node_id
+
+	# send package to correct location/node in firebase
+	s_location = 'Winery'
+	n_url = "%s/%s" % (s_location, node_name)
+	fb.put(n_url, time, fb_payload)
+	
 
 
