66899678d1499895826197991ffafb4f0bf881eb
chadtimmins <chadtimmins@gmail.com>
2015-02-18 19:29:12 -0800

    Added function for handling unknown sensor node data transmission

--- stem/stem.py
+++ stem/stem.py
@@ -13,15 +13,18 @@ from datetime import datetime
 import Adafruit_BBIO.UART as BB_UART
 
 class Stem():
-	def __init__(self, uart='UART2', port='/dev/ttyO2', baud=9600):
+	def __init__(self, uart='UART2', port='/dev/ttyO2', baud=9600, cmds={}):
 		# open uart port
 		BB_UART.setup(uart)
-		print '%s opened on %s' % (uart, port) 
+		print '%s opened on %s\n' % (uart, port)
+
 		# instantiate XBee and serial port objects
 		self.serial = serial.Serial(port, baud)
 		self.xbee = XBee(self.serial)
-		print 'xbee instantiated'
 		self.onStartup()
+
+		# make data structure to hold node settings
+		self.nodes = {}
 
 	def __del__(self):
 		if self.serial.isOpen():
@@ -35,7 +38,32 @@ class Stem():
 		print 'onStartup function calls'
 
 	def onMsgReceive(self, msg):
+		time_stamp = self.getTime()
 		src = msg.get('source_addr')
 		if src != None:
+			# left shift MSB and OR to get src value
 			src = ord(src[0]) << 8 | ord(src[1])
-			print 'source_addr: %s' % src
+			
+			# if source is previously unknown, make new entry for node settings
+			if not src in self.nodes:
+				self.nodes[src] = {'name': '', 'settings': {}}
+				self.updateNodeSettings(src)
+				"""
+				add code for reading settings
+				"""
+		else:
+			print "something wen't seriously wrong"
+
+		# retrieve data and parse
+		data = msg.get('rf_data').split(',')
+
+	def parseData(self, data):
+		"""
+		code for parsing data 
+		"""
+
+	def updateNodeSettings(self, src):
+		print 'updating node settings\n'
+		"""
+		this is where code for updating a node's settings goes
+		"""
